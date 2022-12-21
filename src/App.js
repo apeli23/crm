8d0ceb8bb73b1698178f9879ca5208a10de07098
apeli23 <apelibrian@gmail.com>
2022-12-21 12:08:59 +0300

    themes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
- 
+import { ColorModeContext, useMode } from './theme';
+//the code below gives abilioty to passs in the themes into material UI
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
 function App() {
+  //access to the theme and color mode we created in `theme.js`
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="App">
-      
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline /> {/*resets css to default */}
+      <div className="App"></div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
